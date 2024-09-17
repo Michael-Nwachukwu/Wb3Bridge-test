907d0b0af159fdc7b84c64108cce8782979b5486
Michael Nwachukwu <dahformulah@gmail.com>
2024-09-17 17:50:22 +0100

    updates

---
+++ contracts/Ludo.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.24;
+
+contract Ludo {
+    uint256 gameId;
+    uint256 nonce = 0;
+    uint256 boardNumber = 52;
+    // Ludo board has 52 positions
+    uint[52] public ludoBoard;
+
+
+    struct Game {
+        uint256 id;
+        uint8 numberOfPlayers;
+        string[] colors;
+        address[] players;
+        uint8 nextColorindex;
+    }
+
+    struct Player {
+        string color;
+        bool isActive;
+        bool isTurn;
+        uint position;
+        address player;
+    }
+
+    mapping (address => Player) players;
+    mapping (uint256 => Game) games;
+    mapping (address => mapping (uint256 => bool)) hasJoinedGame;
+
+
+    function createGame(string[] memory _colors) external {
+
+        uint256 _gameId = gameId + 1;    
+        Game storage newGame = games[_gameId];
+
+        newGame.id = _gameId;
+        newGame.colors = _colors;
+
+    }
+
+    function joinGame(uint256 _gameId) external {
+        require(!hasJoinedGame[msg.sender][_gameId], "Player already joined");
+        Game storage game = games[_gameId];
+        require(game.nextColorindex < game.colors.length, "Game is full");
+
+        Player storage newPlayer = players[msg.sender];
+        newPlayer.color = game.colors[game.nextColorindex];
+        newPlayer.player = msg.sender;
+
+        game.players.push(msg.sender);
+        game.nextColorindex++;
+        game.numberOfPlayers++;
+
+        hasJoinedGame[msg.sender][_gameId] = true;
+    }
+
+    function rollDice(address player) internal returns (uint8) {
+
+        uint256 result = uint256(keccak256(abi.encodePacked(
+            block.timestamp,
+            block.prevrandao,
+            player,
+            nonce
+        )));
+        nonce++;
+        return uint8(result % 6) + 1;
+
+    }
+
+    function play() external {
+
+        rollDice(msg.sender);
+        
+    }
+
+
+
+
+}
